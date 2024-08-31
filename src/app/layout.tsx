c74fe6cdfeec31fa02395d91151d0b6d15a5f325
aoba <writer_fairy_road@yahoo.co.jp>
2024-08-31 13:09:23 +0900

    Add modals component. (#1)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
+import { Modals } from '@/components/modals'
+
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -17,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='ja'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Modals />
+        {children}
+      </body>
     </html>
   )
 }
