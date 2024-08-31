fc16c98dedef0e40cd14cdc16e93e84c12bf35ec
aoba <writer_fairy_road@yahoo.co.jp>
2024-08-31 17:58:00 +0900

    💄 Add participating user list ui. (#3)

---
+++ src/mock-data/participating-user-list.ts
@@ -0,0 +1,17 @@
+export type User = {
+  id: number
+  name: string
+}
+
+export const mockParticipationgUserList: User[] = [
+  { id: 1, name: 'test User 1' },
+  { id: 2, name: 'test User 2' },
+  { id: 3, name: 'test User 3' },
+  { id: 4, name: 'test User 4' },
+  { id: 5, name: 'test User 5' },
+  { id: 6, name: 'test User 6' },
+  { id: 7, name: 'test User 7' },
+  { id: 8, name: 'test User 8' },
+  { id: 9, name: 'test User 9' },
+  { id: 10, name: 'test User 10' },
+]
