10f620bdc7cb70132cde19967c4268c7969e3a69
aoba <writer_fairy_road@yahoo.co.jp>
2024-08-31 14:37:02 +0900

    🔥 Add home page and remove loading code.(#1)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,26 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
-import { Project } from '@/mock-data/user-projects'
-import { useCreateProjectModal } from '@/features/project/store/use-create-project-modal'
 import { useRouter } from 'next/navigation'
+
+import { useCreateProjectModal } from '@/features/project/store/use-create-project-modal'
+import { useMockUserProjects } from '@/mock-data/store/use-mock-user-projects'
 
 export default function Home() {
   const router = useRouter()
-  const [isLoading, setIsLoading] = useState<boolean>(false)
-  
+
   const [open, setOpen] = useCreateProjectModal()
 
-  const [userProjects, _] = useState<Project[]>([])
+  const [mockUserProjects, _] = useMockUserProjects()
 
-  const projectId = userProjects[0]?.id
+  const projectId = mockUserProjects[0]?.id
 
   useEffect(() => {
     if (projectId) {
-      router.replace('/test');
+      router.replace(`/projects/${projectId}`)
     } else if (!open) {
       setOpen(true)
     }
-  }, [projectId, open, setOpen])
+  }, [projectId, open, setOpen, router])
 }
