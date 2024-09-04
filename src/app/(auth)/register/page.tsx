011eb5e44be8024cc8076dae1d913ee73ee459d5
aoba <writer_fairy_road@yahoo.co.jp>
2024-09-04 14:54:41 +0900

    💄 Add register page UI. (#17)

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+import { RegisterForm } from '@/features/auth/components/register-fomr'
+
+export default function RegisterPage() {
+  return (
+    <div className='w-96 h-auto flex flex-col'>
+      <RegisterForm />
+    </div>
+  )
+}
