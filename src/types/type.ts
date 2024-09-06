6b9c7e8900730b889a11cfd8d7611c941f27208c
aoba <writer_fairy_road@yahoo.co.jp>
2024-09-06 19:13:22 +0900

    🏷️ Add task and user type.

--- src/types/type.ts
+++ src/types/type.ts
@@ -9,3 +9,22 @@ export type Project = {
   createdAt: string
   updatedAt: string
 }
+
+export type TaskType = {
+  id: number
+  name: string
+  description: string
+  dueDate: string
+  status: 'progress' | 'is_pending' | 'completed'
+  priority: 'low' | 'medium' | 'high'
+  imagePath: string
+  createdBy: number
+  updatedBy: number
+  createdAt: string
+  updatedAt: string
+  assigned_user_id: number
+}
+
+export type UserType = {
+  name: string
+}
